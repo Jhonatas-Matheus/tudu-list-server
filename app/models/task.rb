022cef13dcf76bd0105cc455cecf45b25626c17d
Jhonatas <jhonatasoliveira021@gmail.com>
2023-07-19 09:28:32 -0300

    feat: implementation model of tasks

---
+++ app/models/task.rb
@@ -0,0 +1,13 @@
+class Task
+  include Mongoid::Document
+  include Mongoid::Timestamps
+
+  field :title, type: String
+  field :description, type: String
+  field :deadline, type: Date
+  field :status, type: Integer
+
+  has_and_belongs_to_many :users
+  has_many :comments
+  accepts_nested_attributes_for :comments
+end
